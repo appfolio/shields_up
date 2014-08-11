4f1a61a5344a8cd1caa69016974e26ab51081c33
Johannes Schlumberger <johannes.schlumberger@appfolio.com>
2014-08-11 16:52:05 -0700

    js - small cleanups

--- lib/af_strong_parameters/forbidden_attributes_protection.rb
+++ lib/af_strong_parameters/forbidden_attributes_protection.rb
@@ -4,8 +4,8 @@ module AfStrongParameters
   module ForbiddenAttributesProtection
     def sanitize_for_mass_assignment(*options)
       new_attributes = options.first.keys.map(&:to_sym)
-      permitted_attributes = Thread.current.thread_variable_get(:permitted_for_mass_assignment).try(:fetch, self.class.to_s.underscore.to_sym, [])
-      if Thread.current.thread_variable_get(:permitted_for_mass_assignment).nil? || (new_attributes - permitted_attributes).empty?
+      permitted_attributes = Thread.current.thread_variable_get(:permitted_for_mass_assignment).try(:fetch, self.class.to_s.underscore.to_sym) || []
+      if Thread.current.thread_variable?(:permitted_for_mass_assignment) || (new_attributes - permitted_attributes).empty?
         super
       else
         raise AfStrongParameters::ForbiddenAttributes "#{new_attributes - permitted_attributes} not allowed to be mass-assigned on #{self.class}."
