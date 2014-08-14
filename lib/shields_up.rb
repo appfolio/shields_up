266dc07f6264fa6d5065f556285c9dc623572120
Johannes Schlumberger <johannes.schlumberger@appfolio.com>
2014-08-14 10:15:39 -0700

    js - automatically include controller extensions

--- lib/shields_up.rb
+++ lib/shields_up.rb
@@ -1,3 +1,10 @@
 require 'request_store'
 require 'shields_up/forbidden_attributes_protection'
+require 'shields_up/action_controller_extensions'
+
+if defined? ActionController
+  if defined? ActionController::Base
+    ActionController::Base.send(:include, ShieldsUp::ActionControllerExtensions)
+  end
+end
 
